e1fcc157d8e8c54469de886b67b3e2e8a564bda9
johndo31 <30750064+johndo31@users.noreply.github.com>
2024-11-12 14:51:50 +0100

    Initial Commit

---
+++ ClaudeMailAssistant/ClaudeMailAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeMailAssistantApp.swift
+//  ClaudeMailAssistant
+//
+//  Created by Jan Niklas Sikorra on 12.11.24.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeMailAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeMailAssistantTests/ClaudeMailAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeMailAssistantTests.swift
+//  ClaudeMailAssistantTests
+//
+//  Created by Jan Niklas Sikorra on 12.11.24.
+//
+
+import Testing
+@testable import ClaudeMailAssistant
+
+struct ClaudeMailAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
